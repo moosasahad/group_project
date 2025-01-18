d53f017998cc90fa113ef4d8d31cbbd830426257
moosa sahad <sahadmoosa018@gmail.com>
2025-01-18 10:30:26 +0530

    sum

--- src/App.js
+++ src/App.js
@@ -3,14 +3,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-     <h1>hello world</h1>  
-     <h2>moosa</h2>
-<h2>rinuuu</h2>
-<h3>shahuuuuudsfadsfjadsjfa
-
-</h3>
-<div>gadsgadsgadsg</div>
+    <div>
+      hallow
     </div>
   );
 }
