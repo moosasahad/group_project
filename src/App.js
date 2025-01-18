543b7497184409c18d688a61d957854a4b2d7f00
Aslah7070 <aslah.c7070@gmail.com>
2025-01-18 10:22:31 +0530

    hello

--- src/App.js
+++ src/App.js
@@ -6,11 +6,7 @@ function App() {
     <div className="App">
      <h1>hello world</h1>  
      <h2>moosa</h2>
-<h2>rinuuu</h2>
-<h3>shahuuuuudsfadsfjadsjfa
 
-</h3>
-<div>gadsgadsgadsg</div>
     </div>
   );
 }
